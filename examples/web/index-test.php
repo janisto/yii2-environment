2706fd53e640b2565bee4c154de5e2919a15e350
Jani Mikkonen <jani.mikkonen@whitesheep.fi>
2014-10-27 14:34:57 +0200

    Add examples

--- examples/web/index-test.php
+++ examples/web/index-test.php
@@ -10,3 +10,4 @@ require(dirname(__DIR__) . '/vendor/autoload.php');
 $env = new \janisto\environment\Environment(dirname(__DIR__) . '/config', 'test');
 $env->setup();
 (new yii\web\Application($env->web))->run();
+//$env->showDebug(); // show produced environment configuration
