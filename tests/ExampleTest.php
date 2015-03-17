7615bbaa92aa2eacfc5274660f164ab8c12bbd9b
Jani Mikkonen <jani@mikkonen.info>
2015-03-17 13:07:50 +0200

    Test travis

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,6 +9,6 @@ class ExampleTest extends \PHPUnit_Framework_TestCase
      */
     public function testTrueIsTrue()
     {
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }
